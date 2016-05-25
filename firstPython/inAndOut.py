d0a79533c4ecde8f7a6b36222849d18eadfef20e
pyk <panyk@hdsxtech.com>
2016-05-25 17:04:13 +0800

    git基础学习

---
+++ firstPython/inAndOut.py
@@ -0,0 +1,11 @@
+#python 的输出
+print('输出','多个','字符串，每遇到一个字符串会输出一个空格。')
+
+#python的输入
+print("请输入您的姓名：")
+name = input()
+print("您刚才输入的姓名是=",name)
+
+#在输入之前提示用户
+name=input("请输入您的姓名：")
+print("您的姓名 =",name)
